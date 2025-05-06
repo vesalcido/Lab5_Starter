3c8016ae41dd59d360643213d3da1995332780e1
vesalcido <vesalcido@ucsd.edu>
2025-05-06 12:35:58 -0700

    Made the voice list

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -8,4 +8,21 @@ function init() {
   const playButton = document.querySelector('button');
   const textInput = document.getElementById('text-to-speak');
   const faceImage = document.querySelector('#explore img');
+
+  const synth = window.speechSynthesis;
+ let voices = [];
+
+
+ // made the voice list using the speech synthesis documentation
+ function populateVoiceList() {
+   voices = synth.getVoices();
+
+   voices.forEach((voice, index) => {
+     const option = document.createElement('option');
+     option.textContent = `${voice.name} (${voice.lang})`;
+     option.value = index;
+     voiceSelection.appendChild(option);
+   });
+ }
+
 }
