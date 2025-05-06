765bcc6d3e8aeb0e82abf894f965ecfdb08d877e
khanggn <khn042@ucsd.edu>
2025-05-06 13:15:08 -0700

    finish test cases 1 true and 1 false for isPhoneNumber function

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,12 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+//Test for the function isPhoneNumber
+//true 
+test('valid phone number: 619-315-8598', () => {
+  expect(isPhoneNumber('619-315-8598')).toBe(true);
+});
+//false
+test('valid phone number: 61-9315-8598', () => {
+  expect(isPhoneNumber('61-9315-8598')).toBe(false);
+});
