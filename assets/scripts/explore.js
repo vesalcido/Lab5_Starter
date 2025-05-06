931a43309a591040c7737f23ddcb9d95ce82900a
khanggn <khn042@ucsd.edu>
2025-05-06 12:42:39 -0700

    added the handler for button

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,25 +4,38 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   //Created the varaibles for the components on html
-  const voiceSelection = document.getElementById('voice-select');
-  const playButton = document.querySelector('button');
-  const textInput = document.getElementById('text-to-speak');
-  const faceImage = document.querySelector('#explore img');
+  const voiceSelection=document.getElementById('voice-select');
+  const playButton=document.querySelector('button');
+  const textInput=document.getElementById('text-to-speak');
+  const faceImage=document.querySelector('#explore img');
 
   const synth = window.speechSynthesis;
  let voices = [];
 
 
  // made the voice list using the speech synthesis documentation
- function populateVoiceList() {
+ function populateVoiceList(){
    voices = synth.getVoices();
 
-   voices.forEach((voice, index) => {
-     const option = document.createElement('option');
-     option.textContent = `${voice.name} (${voice.lang})`;
-     option.value = index;
+   voices.forEach((voice, index)=>{
+     const option=document.createElement('option');
+     option.textContent=`${voice.name} (${voice.lang})`;
+     option.value=index;
      voiceSelection.appendChild(option);
    });
  }
+  synth.onvoiceschanged=populateVoiceList;
+  populateVoiceList();
+  //functionaility of the button when the user clicks it
+  playButton.addEventListener('click',()=>{
+    const text=textInput.value.trim();
+    if(!text)return;
+    const utterance=new SpeechSynthesisUtterance(text);
+    const selectedIndex=voiceSelection.value;
+    if(selectedIndex!=="select"){
+      utterance.voice=voices[selectedIndex];
+    }
 
+
+  });
 }
