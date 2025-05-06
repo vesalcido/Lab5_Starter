b04d4ebcf28d9121fce56e9f79a76efff2aee453
khanggn <khn042@ucsd.edu>
2025-05-06 12:20:43 -0700

    created components variables

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,9 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  //Created the varaibles for the components on html
+  const voiceSelection = document.getElementById('voice-select');
+  const playButton = document.querySelector('button');
+  const textInput = document.getElementById('text-to-speak');
+  const faceImage = document.querySelector('#explore img');
 }
